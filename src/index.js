864ab61710586a13b8dbf35a639c5c7edb7cc1bc
Dimadzu <Dimadzu@i.ua>
2021-03-08 00:41:06 +0300

    feat: add function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array==undefined){
+		return 0;
+	}else	if (array.length!==0){
+	return Math.min(...array);
+	} else {
+		return 0;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array==undefined){
+		return 0;
+	}else  if (array.length!==0){
+	return (Math.max(...array));
+	} else {
+		return 0;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	let avge=0;
+	for (let i=0;i<array.length;i++){
+		avge+=array[i];
+		
+		
+	}
+  if(array==undefined){
+		return 0;
+	}else if(array.length!==0){
+	return(avge/array.length);}
+	else { return 0;}
+  
 }
